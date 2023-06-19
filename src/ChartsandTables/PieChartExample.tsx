8c3a8d68703c452421fa58a58f3e539fa2ea0315
Aditya Banerjee <aditya04banerjee@gmail.com>
2023-06-20 00:42:29 +0530

    Resolve eslint warnings

--- src/ChartsandTables/PieChartExample.tsx
+++ src/ChartsandTables/PieChartExample.tsx
@@ -2,7 +2,6 @@ import { Typography } from "@mui/material";
 import {
   PieChart,
   Pie,
-  Sector,
   Cell,
   ResponsiveContainer,
   Tooltip,
@@ -17,8 +16,6 @@ const data = [
 ];
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
-const RADIAN = Math.PI / 180;
 
 export const PieChartExample = () => {
   return (
