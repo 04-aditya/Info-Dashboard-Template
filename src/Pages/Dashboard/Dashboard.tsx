c35a4e2cae899297bfad7b0cbeb7ba519a1858d2
Aditya Banerjee <aditya04banerjee@gmail.com>
2023-06-20 23:55:31 +0530

    Add basic card to Dashboard.tsx

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,38 @@
-import { Toolbar } from "@mui/material";
+import { Card, Toolbar, Avatar } from "@mui/material";
 import "./Dashboard.css";
+import { StackedAreaChartExample } from "../../ChartsandTables/StackedAreaChartExample";
+import { gitAvatar } from "../../LinksandUsernames";
 
 export const Dashboard = () => {
   return (
     <div className="Dashboard">
       <Toolbar />
-      <div>Dashboard Page</div>
+      <Card
+        sx={{
+          width: "90%",
+          margin: "15px",
+          padding: "15px",
+          boxShadow: "9",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "15px",
+        }}
+      >
+        <Avatar
+          sx={{
+            height: "110px",
+            width: "110px",
+
+            boxShadow: "9",
+            "&:hover": {
+              transform: "scale(1.1)",
+            },
+          }}
+          src={gitAvatar}
+        />
+        Aditya Banerjee
+      </Card>
     </div>
   );
 };
