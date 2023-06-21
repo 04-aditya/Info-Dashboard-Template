3afe99ea4e725a7407fb1b6d246bf980a3e7ac30
Aditya Banerjee <aditya04banerjee@gmail.com>
2023-06-21 05:46:20 +0530

    Update Home.tsx and delete CustomButton.tsx

--- src/Components/CustomButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@mui/material"
-
-export const CustomButton: any = () => {
-    return(
-        <Button>Click ME
-            
-        </Button>
-    )
-}
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,54 @@
+import { Button, Typography } from "@mui/material";
 import "./Home.css";
+import { Link } from "react-router-dom";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div className="Home">
+      <Typography>
+        This project is an attempt to make a basic template of a dashboard
+        design to showcase various information through tabs and cards.{" "}
+      </Typography>
+      <br />
+      <Typography>
+        It includes data about several professional profiles (eg. Github and
+        LinkedIn profiles).
+      </Typography>
+      <br />
+      <br />
+
+      <Typography>
+        Feel free to navigate around using the sidebar. You'll find some dummy
+        charts and tables scattered around as well.
+      </Typography>
+      <Typography>
+        In case of any problems, please go through the README of this project.
+      </Typography>
+      <br />
+      <br />
+      <Link
+        to={`https://github.com/04-aditya/Info-Dashboard-Template`}
+        target="_blank"
+      >
+        <Button
+          variant="contained"
+          sx={{
+            borderRadius: "30px",
+
+            height: "fit-content",
+            background: "#2b3137",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#39D0B8",
+              color: "black",
+            },
+          }}
+        >
+          <GitHubIcon sx={{ marginRight: "10px" }} />
+          Project Repo
+        </Button>
+      </Link>
+    </div>
+  );
 };
